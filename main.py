91018b4b930fa47aae6516447a283d70e3e4ae4f
Danger Dane <gandalf12298@gmail.com>
2022-03-30 20:06:48 +0000

    small details

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import discord
 import random
 
+REPO_LINK = "https://github.com/dane9640/Daneger-Bot"
+
 client = discord.Client()
 
 @client.event
@@ -21,6 +23,10 @@ async def on_message(message):
     await message.channel.send(f"Your random number is: {random.randrange(100)}")
 
   elif(userMessage.lower() == "!intro"):
-    await message.channel.send(f"Hello! I am a discord bot that Dane is making to get better at python/API's more features are incoming")
+    await message.channel.send(f"Hello! I am a discord bot that Dane is making to get better at python/API's more features are incoming.")
+    await message.channel.send(f"You can find the source code at: {REPO_LINK}")
 
+  elif(userMessage.lower() == "!commands"):
+    await message.channel.send(f"Current commands (More will be added)\n !rand: gives random number\n !intro: I introduce myself\n !commands: You should know what this does")
+  
 client.run(os.environ['discordToken'])
